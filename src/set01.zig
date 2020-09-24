f937e666b62401e7de720fa29b39151754a9df27
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-09-24 14:06:40 -0600

    Break repeating XOR key :D

--- src/set01.zig
+++ src/set01.zig
@@ -37,24 +37,17 @@ test "challenge 2: fixed xor" {
 
 const SingleByteXorBreak = struct {
     allocator: *std.mem.Allocator,
-    ciphertext: []u8,
     key: u8,
     cleartext: []u8,
     cipher_char_appearances: []freq.CharacterElement,
 
     pub fn deinit(this: @This()) void {
-        this.allocator.free(this.ciphertext);
         this.allocator.free(this.cleartext);
         this.allocator.free(this.cipher_char_appearances);
     }
 };
 
-fn break_single_byte_xor(allocator: *std.mem.Allocator, hex_text: []const u8) !SingleByteXorBreak {
-
-    // Convert ciphertext from hex to bytes
-    var ciphertext = try fmt.hexToBytes(allocator, hex_text);
-    errdefer allocator.free(ciphertext);
-
+fn break_single_byte_xor(allocator: *std.mem.Allocator, ciphertext: []const u8) !SingleByteXorBreak {
     // Get the list of characters used by frequency
     const characters_by_frequency = try freq.characters_by_frequency(allocator, ciphertext);
     errdefer allocator.free(characters_by_frequency);
@@ -71,17 +64,23 @@ fn break_single_byte_xor(allocator: *std.mem.Allocator, hex_text: []const u8) !S
 
     return SingleByteXorBreak{
         .allocator = allocator,
-        .ciphertext = ciphertext,
         .cipher_char_appearances = characters_by_frequency,
         .key = key,
         .cleartext = cleartext,
     };
 }
 
+fn break_single_byte_xor_hex(allocator: *std.mem.Allocator, hex_text: []const u8) !SingleByteXorBreak {
+    // Convert ciphertext from hex to bytes
+    var ciphertext = try fmt.hexToBytes(allocator, hex_text);
+    defer allocator.free(ciphertext);
+    return break_single_byte_xor(allocator, ciphertext);
+}
+
 test "challenge 3: single-byte xor cipher" {
     const allocator = std.testing.allocator;
 
-    var xor_break = try break_single_byte_xor(allocator, "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+    var xor_break = try break_single_byte_xor_hex(allocator, "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
     defer xor_break.deinit();
 
     std.testing.expectEqualSlices(u8, "Cooking MC's like a pound of bacon", xor_break.cleartext);
@@ -98,7 +97,7 @@ test "challenge 4: detect single-byte xor cipher" {
     // Loop through each line of the text files
     line_loop: while (line_iter.next()) |line| {
         // Try to break the hex-encoded string with a single byte-xor breaker
-        var xor_break = try break_single_byte_xor(allocator, line);
+        var xor_break = try break_single_byte_xor_hex(allocator, line);
         defer xor_break.deinit();
 
         // Check that each character in decrypted string is an english character
@@ -177,4 +176,98 @@ pub fn hamming_distance(s1: []const u8, s2: []const u8) usize {
 test "hamming distance" {
     std.testing.expectEqual(@as(usize, 37), hamming_distance("this is a test", "wokka wokka!!!"));
     std.testing.expectEqual(@as(usize, 8), hamming_distance(&[_]u8{0xFF}, &[_]u8{0x00}));
+}
+
+test "challenge 6: break repeating-key XOR" {
+    const Base64DecoderWithIgnore = std.base64.Base64DecoderWithIgnore;
+
+    const allocator = std.testing.allocator;
+    const base64_decoder = Base64DecoderWithIgnore.init(std.base64.standard_alphabet_chars, std.base64.standard_pad_char, " \n\r");
+
+    // Read ciphertext into a raw byte sequence
+    const ciphertext_base64 = @embedFile("./set1challenge6.txt");
+
+    var ciphertext_buf: [Base64DecoderWithIgnore.calcSizeUpperBound(ciphertext_base64.len)]u8 = undefined;
+    const decoded_len = try base64_decoder.decode(&ciphertext_buf, ciphertext_base64);
+
+    const ciphertext = ciphertext_buf[0..decoded_len];
+
+    // Find the keysize that is most likely to be correct
+    const keysize = find_keysize_with_min_edit_dist: {
+        const f64_plus_infinity: u64 = 0x7FF0000000000000;
+
+        var min_edit_dist: f64 = @bitCast(f64, f64_plus_infinity);
+        var keysize_with_min_edit_dist: usize = undefined;
+
+        var keysize: usize = 4;
+        while (keysize < 40) : (keysize += 1) {
+            var total_edit_distance: usize = 0;
+
+            total_edit_distance += hamming_distance(ciphertext[0 * keysize ..][0..keysize], ciphertext[1 * keysize ..][0..keysize]);
+            total_edit_distance += hamming_distance(ciphertext[1 * keysize ..][0..keysize], ciphertext[2 * keysize ..][0..keysize]);
+            total_edit_distance += hamming_distance(ciphertext[2 * keysize ..][0..keysize], ciphertext[3 * keysize ..][0..keysize]);
+
+            const average_edit_dist = @intToFloat(f64, total_edit_distance) / 3;
+            const normalized_edit_dist = average_edit_dist / @intToFloat(f64, keysize * 8);
+
+            if (normalized_edit_dist < min_edit_dist) {
+                min_edit_dist = normalized_edit_dist;
+                keysize_with_min_edit_dist = keysize;
+            }
+        }
+
+        break :find_keysize_with_min_edit_dist keysize_with_min_edit_dist;
+    };
+
+    // Transpose the ciphertext into keysize parts
+    const transpose_block_size = (ciphertext.len - 1) / keysize + 1;
+    var ciphertext_transpose = try allocator.alloc(u8, ciphertext.len);
+    defer allocator.free(ciphertext_transpose);
+
+    var transpose_block: usize = 0;
+    while (transpose_block < keysize) : (transpose_block += 1) {
+        var idx_in_transpose_block: usize = 0;
+        while (idx_in_transpose_block < transpose_block_size) : (idx_in_transpose_block += 1) {
+            const transpose_idx = transpose_block * transpose_block_size + idx_in_transpose_block;
+            const ciphertext_idx = idx_in_transpose_block * keysize + transpose_block;
+
+            if (ciphertext_idx >= ciphertext.len) continue;
+            if (transpose_idx >= ciphertext_transpose.len) continue;
+
+            ciphertext_transpose[transpose_idx] = ciphertext[ciphertext_idx];
+        }
+    }
+
+    // Break each transposed block as a single byte XOR
+    var single_byte_xor_breaks = try allocator.alloc(SingleByteXorBreak, keysize);
+    defer {
+        for (single_byte_xor_breaks) |xor_break| {
+            xor_break.deinit();
+        }
+        allocator.free(single_byte_xor_breaks);
+    }
+
+    transpose_block = 0;
+    while (transpose_block < keysize) : (transpose_block += 1) {
+        const start_of_block = transpose_block * transpose_block_size;
+        const end_of_block = std.math.min((transpose_block + 1) * transpose_block_size, ciphertext_transpose.len);
+
+        single_byte_xor_breaks[transpose_block] = try break_single_byte_xor(allocator, ciphertext_transpose[start_of_block..end_of_block]);
+    }
+
+    var key = try allocator.alloc(u8, keysize);
+    defer allocator.free(key);
+
+    for (single_byte_xor_breaks) |xor_break, idx| {
+        key[idx] = xor_break.key;
+    }
+    std.log.warn("key = {X}\n", .{key});
+    std.log.warn("key = \"{}\"\n", .{key});
+
+    var cleartext = try std.mem.dupe(allocator, u8, ciphertext);
+    defer allocator.free(cleartext);
+
+    xor.repeating_xor_slice_in_place(cleartext, key);
+
+    std.debug.warn("cleartext: {}\n", .{cleartext});
 }
