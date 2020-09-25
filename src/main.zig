7cf23aeeb1466339ea6eaf97c1233ea24e9c6e2d
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-09-25 17:24:13 -0600

    Complete set 1 challenge 8: dectect AES128 ECB

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,8 @@ const AES128 = std.crypto.core.aes.AES128;
 const MAX_FILE_SIZE = 50 * 1000 * 1000;
 
 const CMD_DECRYPT_AES128_ECB = "decrypt-aes128-ecb";
-const HELP_LIST_SUBCOMMANDS = "  " ++ CMD_DECRYPT_AES128_ECB ++ "\n";
+const CMD_DETECT_AES128_ECB = "detect-aes128-ecb";
+const HELP_LIST_SUBCOMMANDS = "  " ++ CMD_DECRYPT_AES128_ECB ++ "\n  " ++ CMD_DETECT_AES128_ECB ++ "\n";
 
 pub fn main() !void {
     var gpa_allocator = std.heap.GeneralPurposeAllocator(.{}){};
@@ -23,6 +24,8 @@ pub fn main() !void {
 
     if (std.mem.eql(u8, CMD_DECRYPT_AES128_ECB, subcommand_str)) {
         try decrypt_aes128_ebc(allocator, &args_iter);
+    } else if (std.mem.eql(u8, CMD_DETECT_AES128_ECB, subcommand_str)) {
+        try detect_aes128_ebc(allocator, &args_iter);
     } else {
         std.debug.warn(
             \\Unknown subcommand "{}".
@@ -73,5 +76,46 @@ pub fn decrypt_aes128_ebc(allocator: *std.mem.Allocator, args_iter: *std.process
     while (index < ciphertext.len) : (index += plaintext.len) {
         aes.decrypt(&plaintext, ciphertext[index..]);
         _ = try stdout.write(&plaintext);
+    }
+}
+
+pub fn detect_aes128_ebc(allocator: *std.mem.Allocator, args_iter: *std.process.ArgIterator) !void {
+    const filepath = try args_iter.next(allocator) orelse {
+        std.debug.warn("Pass in file with hex encoded ciphertexts\n", .{});
+        return;
+    };
+    defer allocator.free(filepath);
+
+    // Read ciphertext into a raw byte sequence
+    const cwd = std.fs.cwd();
+    const file_contents = try cwd.readFileAlloc(allocator, filepath, MAX_FILE_SIZE);
+    defer allocator.free(file_contents);
+
+    const stdout = std.io.getStdOut().writer();
+
+    var line_iter = std.mem.tokenize(file_contents, "\n\r");
+    var line_num: usize = 0;
+    while (line_iter.next()) |line| {
+        line_num += 1;
+
+        var line_bytes = try allocator.alloc(u8, (line.len + 1) / 2);
+        defer allocator.free(line_bytes);
+
+        try std.fmt.hexToBytes(line_bytes, line);
+
+        const AES_BLOCK_SIZE = 16;
+
+        var seen_blocks = std.AutoHashMap([AES_BLOCK_SIZE]u8, void).init(allocator);
+        defer seen_blocks.deinit();
+
+        var index: usize = 0;
+        while (index < line_bytes.len) : (index += AES_BLOCK_SIZE) {
+            const block = line_bytes[index..][0..AES_BLOCK_SIZE];
+
+            const gop = try seen_blocks.getOrPut(block.*);
+            if (gop.found_existing) {
+                try stdout.print("Found repeated section on line {}: {X}\n", .{ line_num, block });
+            }
+        }
     }
 }
