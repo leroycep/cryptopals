b532c2bbf0b0000d239c70376409cf24e33cdee6
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-09-25 16:37:28 -0600

    Use zig std lib to decrypt AES 128 bit ECB

--- build.zig
+++ build.zig
@@ -3,9 +3,12 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) !void {
     const print_character_frequencies = b.addExecutable("print_character_frequencies", "src/print_character_frequencies.zig");
     b.step("print_character_frequencies", "build the print_character_frequencies binary").dependOn(&print_character_frequencies.run().step);
-    
+
     const cryptopal = b.addExecutable("cryptopal", "src/main.zig");
     cryptopal.install();
-    
+
     b.step("run", "run the cryptopal binary").dependOn(&cryptopal.run().step);
+
+    const test_step = b.step("test", "test stuff");
+    test_step.dependOn(&b.addTest("src/main.zig").step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+const AES128 = std.crypto.core.aes.AES128;
+
+const MAX_FILE_SIZE = 50 * 1000 * 1000;
 
 pub fn main() !void {
     var gpa_allocator = std.heap.GeneralPurposeAllocator(.{}){};
@@ -10,19 +13,72 @@ pub fn main() !void {
     std.debug.assert(args_iter.skip());
 
     const subcommand_str = try args_iter.next(allocator) orelse {
-        print_help_message();
+        std.debug.warn(
+            \\ Subcommands:
+            \\   decrypt
+            \\
+        , .{});
         return;
     };
     defer allocator.free(subcommand_str);
-}
 
-pub fn print_help_message() void {
-    std.debug.warn(
-        \\ Usage:
-        \\   cryptopal <subcommand>
-        \\
-        \\ Subcommands:
-        \\   <none>
-        \\
-    , .{});
+    if (std.mem.eql(u8, "decrypt", subcommand_str)) {
+        const method_str = try args_iter.next(allocator) orelse {
+            std.debug.warn(
+                \\ Supported algorithms:
+                \\   aes128-ecb
+                \\
+            , .{});
+            return;
+        };
+        defer allocator.free(method_str);
+
+        const filepath = try args_iter.next(allocator) orelse {
+            std.debug.warn(
+                \\ Pass in a base64 file to decrypt
+                \\
+            , .{});
+            return;
+        };
+        defer allocator.free(filepath);
+
+        const key_str = try args_iter.next(allocator) orelse {
+            std.debug.warn(
+                \\ Pass in a key
+                \\
+            , .{});
+            return;
+        };
+        defer allocator.free(key_str);
+
+        const Base64DecoderWithIgnore = std.base64.Base64DecoderWithIgnore;
+
+        const base64_decoder = Base64DecoderWithIgnore.init(std.base64.standard_alphabet_chars, std.base64.standard_pad_char, " \n\r");
+
+        // Read ciphertext into a raw byte sequence
+        const cwd = std.fs.cwd();
+        const ciphertext_base64 = try cwd.readFileAlloc(allocator, filepath, MAX_FILE_SIZE);
+        defer allocator.free(ciphertext_base64);
+
+        var ciphertext_buf = try allocator.alloc(u8, Base64DecoderWithIgnore.calcSizeUpperBound(ciphertext_base64.len));
+        defer allocator.free(ciphertext_buf);
+
+        const decoded_len = try base64_decoder.decode(ciphertext_buf, ciphertext_base64);
+
+        const ciphertext = ciphertext_buf[0..decoded_len];
+
+        if (std.mem.eql(u8, "aes128-ecb", method_str)) {
+            const stdout = std.io.getStdOut().writer();
+
+            const key = key_str[0..16];
+            const aes = AES128.init(key.*);
+
+            var plaintext: [16]u8 = undefined;
+            var index: usize = 0;
+            while (index < ciphertext.len) : (index += plaintext.len) {
+                aes.decrypt(&plaintext, ciphertext[index..]);
+                _ = try stdout.write(&plaintext);
+            }
+        }
+    }
 }
